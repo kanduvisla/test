01263ca32f854aff032a21b652c857d1f8ba8943
Giel Berkers <giel@happy-online.nl>
2017-05-22 14:18:04 +0200

    Added some build and watch stuff

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ var markdown   = require('metalsmith-markdown');
 var layouts    = require('metalsmith-layouts');
 var watch      = require('metalsmith-watch');
 
-Metalsmith(__dirname)
+var site = Metalsmith(__dirname)
   .source('app')
   .destination('build')
-  .use(watch({
+  .use(markdown())
+  .use(layouts({
+    engine: 'handlebars'
+  }));
+
+if (process.argv.indexOf('--watch') !== -1) {
+  site.use(watch({
     paths     : {
       "${source}/**/*": true
     },
     livereload: true
-  }))
-  .use(markdown())
-  .use(layouts({
-    engine: 'handlebars'
-  }))
-  .build(function (err) {
-    if (err) {
-      throw err;
-    }
+  }));
+}
 
-    console.log('Build Succeeded');
-  });
+site.build(function (err) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Build Succeeded');
+});
