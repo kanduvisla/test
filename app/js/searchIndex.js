39ad9bcf0487292f78d78b2cfe40f24400c3857d
Giel Berkers <giel@happy-online.nl>
2017-05-23 16:55:45 +0200

    Search styling and stuff

--- app/js/searchIndex.js
+++ app/js/searchIndex.js
@@ -15,9 +15,10 @@ function SearchIndex() {
   }.bind(this);
   request.send(null);
 
-  this.totalResults = ko.observable(0);
-  this.results      = ko.observable([]);
-  this.reference    = {};
+  this.totalResults  = ko.observable(0);
+  this.results       = ko.observable([]);
+  this.selectedIndex = ko.observable(0);
+  this.reference     = {};
 }
 
 /**
@@ -38,8 +39,34 @@ SearchIndex.prototype.searchIndexLoaded = function (request) {
  * @returns {boolean}
  */
 SearchIndex.prototype.performSearch = function (obj, event) {
-  var searchResults = window.idx.search(event.target.value);
-  this.totalResults(searchResults.length);
-  this.results(searchResults);
+  // 40=down, 38=up, 13=enter
+  var idx = this.selectedIndex();
+
+  if (event.keyCode == 13) {
+    // Go to page:
+    window.location = document.querySelector('.search-result--active a').href;
+  } else if (event.keyCode == 38) {
+    // Up
+    if (idx > 0) {
+      idx -= 1;
+    } else {
+      idx = this.results().length - 1;
+    }
+    this.selectedIndex(idx);
+  } else if (event.keyCode == 40) {
+    // Down:
+    if (idx < this.results().length - 1) {
+      idx += 1;
+    } else {
+      idx = 0;
+    }
+    this.selectedIndex(idx);
+  }
+  else {
+    var searchResults = window.idx.search(event.target.value);
+    this.totalResults(searchResults.length);
+    this.selectedIndex(0);
+    this.results(searchResults);
+  }
   return true;
 }
