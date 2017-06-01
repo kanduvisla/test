8017ea89827846d5645988fe690cad0b69d4e39f
Giel Berkers <giel@happy-online.nl>
2017-06-01 11:32:15 +0200

    mage 2 helper

--- app/js/mage-helper.js
+++ app/js/mage-helper.js
@@ -13,13 +13,27 @@
   }
 
   /**
-   *
    * @param obj
    * @param event
    */
   MageHelper.prototype.changeVersion = function (obj, event) {
     this.articles(articleData[event.target.value]);
     console.log(this.articles());
+  }
+
+  /**
+   * @param obj
+   * @param event
+   */
+  MageHelper.prototype.loadArticle = function (obj, event) {
+    var request                = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("mage-answer").innerHTML = request.responseText;
+      }
+    }
+    request.open('GET', '/' + event.target.value, true);
+    request.send();
   }
 
   ko.applyBindings(new MageHelper(), document.querySelector('.mage-helper'));
